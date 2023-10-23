094fc6f7a5c9b7e28e6f23ac9616bfbcc62cddc9
ZeppIine <kim0904min@gmail.com>
2023-10-23 15:50:35 +0900

    override onClick

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w07intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w07intentdata/ImageActivity.kt
@@ -1,22 +1,57 @@
 package kr.ac.kumoh.ce.s20180147.s23w07intentdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
 import kr.ac.kumoh.ce.s20180147.s23w07intentdata.databinding.ActivityImageBinding
 
-class ImageActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity(), OnClickListener {
+    companion object{
+        const val TANK_NAME = "tank name"
+        const val TANK_RESULT = "tank result"
+
+        const val LIKE = 100
+        const val HATE = 101
+        const val NONE = 0
+    }
+
     private lateinit var main:ActivityImageBinding
+    private lateinit var tanks:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
         main = ActivityImageBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        val res = when(intent.getStringExtra(MainActivity.KEY_NAME)){
-            "amx50b" -> R.drawable.amx50b
-            "amxm454" -> R.drawable.amxm454
+        tanks = intent.getStringExtra(MainActivity.KEY_NAME) ?: "NULL"
+
+        val res = when(tanks){
+            MainActivity.AMX50B -> R.drawable.amx50b
+            MainActivity.AMXM454 -> R.drawable.amxm454
             else -> R.drawable.ic_launcher_foreground
         }
         main.img1.setImageResource(res)
+    }
+
+    override fun onClick(p: View?) {
+        val value = when(p?.id){
+            main.btnlike.id -> LIKE
+            main.btnhate.id -> HATE
+            else -> NONE
+        }
+        val lahe = when(p?.id){
+            main.btnlike.id -> "개추"
+            main.btnhate.id -> "비추"
+            else -> "error"
+        }
+        val intent = Intent()
+        Toast.makeText(this, lahe, Toast.LENGTH_SHORT).show()
+        intent.putExtra(TANK_NAME, tanks)
+        intent.putExtra(TANK_RESULT, lahe)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w07intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w07intentdata/MainActivity.kt
@@ -3,12 +3,16 @@ package kr.ac.kumoh.ce.s20180147.s23w07intentdata
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
 import kr.ac.kumoh.ce.s20180147.s23w07intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     companion object{
         const val KEY_NAME = "tank"
+        const val AMX50B = "amx50b"
+        const val AMXM454 = "amxm454"
     }
     private lateinit var main:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,20 +21,25 @@ class MainActivity : AppCompatActivity() {
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        main.btn1.setOnClickListener {
-            Toast.makeText(this, "무장공비", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, "amx50b")
-            main.text1.setText("AMX 50B")
-            startActivity(intent)
-        }
+        main.btn1.setOnClickListener(this)
+        main.btn2.setOnClickListener(this)
+    }
 
-        main.btn2.setOnClickListener {
-            Toast.makeText(this, "빵헤비", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, "amxm454")
-            main.text1.setText("AMX M4 54")
-            startActivity(intent)
+    override fun onClick(v: View?) {
+        val value = when(v?.id){
+            main.btn1.id -> AMX50B
+            main.btn2.id -> AMXM454
+            else -> return
         }
+        val tank_text = when(v?.id){
+            main.btn1.id -> "AMX 50B"
+            main.btn2.id -> "AMX M4 54"
+            else -> "error"
+        }
+        Toast.makeText(this, tank_text, Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, ImageActivity::class.java)
+        intent.putExtra(KEY_NAME, value)
+        main.text1.setText(tank_text)
+        startActivity(intent)
     }
 }
